0384575f13f77a724ae13b3f00e250fac9de035d
John Lu <z52lu@uwaterloo.ca>
2025-11-17 15:25:39 -0500

    add sizeoracle metrics in meta info script (REF #4)

--- smt_assist/scripts/meta_info.py
+++ smt_assist/scripts/meta_info.py
@@ -1,9 +1,10 @@
+import warnings
 from pathlib import Path
 
 import typer
 
 import pysmt.smtlib.commands as smtcmd
-from pysmt.oracles import get_logic
+from pysmt.oracles import SizeOracle, get_logic
 from pysmt.smtlib.parser import SmtLibParser
 
 app = typer.Typer()
@@ -37,18 +38,34 @@ def main(smt_lib_path: str = typer.Argument(help="Path to SMT-LIB file")):
     pysmt_logic = get_logic(f)
     print(f"logic (identified by PySMT): {pysmt_logic}")
 
-    # commands = list(script) if hasattr(script, "__iter__") else getattr(script, "commands", [])
-    # print(f"\nNumber of commands: {len(commands)}")
-
-    # num_asserts = sum(1 for cmd in commands if getattr(cmd, "name", None) == "assert")
+    # script level info
+    num_asserts = script.count_command_occurrences(smtcmd.ASSERT)
     # num_declares = sum(1 for cmd in commands if getattr(cmd, "name", None) == "declare-fun")
     # num_defines = sum(1 for cmd in commands if getattr(cmd, "name", None) == "define-fun")
     # num_checks = sum(1 for cmd in commands if getattr(cmd, "name", None) == "check-sat")
 
-    # print(f"Number of assertions: {num_asserts}")
+    print(f"Number of assertions: {num_asserts}")
     # print(f"Number of function declarations: {num_declares}")
     # print(f"Number of function definitions: {num_defines}")
     # print(f"Number of check-sat commands: {num_checks}")
+
+    # formula level info
+    # Followings are 6 size metrics from PySMT SizeOracle
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=UserWarning, module="pysmt")
+        tree_node_size = f.size(SizeOracle.MEASURE_TREE_NODES)
+        dag_node_size = f.size(SizeOracle.MEASURE_DAG_NODES)
+        tree_leaf_size = f.size(SizeOracle.MEASURE_LEAVES)
+        depth = f.size(SizeOracle.MEASURE_DEPTH)
+        symbol_size = f.size(SizeOracle.MEASURE_SYMBOLS)
+        bool_dag_size = f.size(SizeOracle.MEASURE_BOOL_DAG)
+
+    print(f"Number of nodes in the formula seen as a tree: {tree_node_size}")
+    print(f"Number of nodes in the formula seen as a DAG: {dag_node_size}")
+    print(f"Number of leaves in the formula seen as a tree: {tree_leaf_size}")
+    print(f"Depth of the formula: {depth}")
+    print(f"Number of different symbols in the formula: {symbol_size}")
+    print(f"Number of nodes in the formula seen as a boolean DAG: {bool_dag_size}")
 
 
 if __name__ == "__main__":
